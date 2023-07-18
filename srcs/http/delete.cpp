28ecad1256f4aeb5cd474c96d5bff17e527e9750
Lucas Fads <lucasfads@gmail.com>
2023-07-18 17:04:41 -0300

    feat: delete method working for deleting files

---
+++ srcs/http/delete.cpp
@@ -0,0 +1,29 @@
+#include "delete.hpp"
+#include "error_codes.hpp"
+#include <fstream>
+#include <string>
+#include <dirent.h>
+#include <unistd.h>
+#include <iostream>
+
+std::string	delete_method(const std::string& path, const std::string& root) {
+	std::string		open_string(root + path);
+	DIR*			dir;
+
+	if ( (dir = opendir(open_string.c_str())) ) {
+		closedir(dir);
+		open_string += "/index.html";
+	}
+	if (access(open_string.c_str(), F_OK))
+		throw NotFoundException();
+	else if (access(open_string.c_str(), W_OK))
+		throw ForbiddenException();
+	std::string		response = ("HTTP/1.1 200 OK\r\n\r\n"); // 200 VS 204?
+	try {
+		std::remove(open_string.c_str());
+	}
+	catch (const std::exception& e) {
+		throw InternalServerErrorException();
+	}
+	return (response);
+}
---
+++ srcs/http/delete.hpp
@@ -0,0 +1,8 @@
+#ifndef DELETE_HPP
+# define DELETE_HPP
+
+#include <string>
+
+std::string	delete_method(const std::string& path, const std::string& root);
+
+#endif
