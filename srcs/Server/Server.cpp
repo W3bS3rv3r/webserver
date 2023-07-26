07790ad779633c9e15fb36f62ed93629eb1dfa02
GabrielCLC <gabrielclcardoso@gmail.com>
2023-07-26 11:11:36 -0300

    refactor: removed extra functions from Server

--- srcs/Server/Server.cpp
+++ srcs/Server/Server.cpp
@@ -22,19 +22,6 @@ Server::~Server(void) {
 	}
 }
 
-//HELPER FUNTIONS
-namespace {
-	inline bool	pendingSocket(int events, int revents) {
-		return (revents & POLLIN && events == POLLIN);
-	}
-	inline bool pendingRead(int events, int revents) {
-		return (revents & POLLIN && events == (POLLIN | POLLOUT));
-	}
-	inline bool pendingWrite(int events, int revents) {
-		return (revents & POLLIN && events == (POLLIN | POLLOUT));
-	}
-}
-
 //METHODS
 void	Server::addSocket(unsigned short port, std::string root) {
 	Socket*	socket = NULL;
@@ -53,17 +40,16 @@ void	Server::addSocket(unsigned short port, std::string root) {
 }
 
 void	Server::init(std::string file) {
+	/*FILE INTERPRETATION HERE ON THE FUTURE*/
     const char* user_name = getlogin();
-
 	if (!user_name)
 		throw Server::NoUserException();
 	std::string path = "/home/";
     path += user_name;
     path += "/webserver";
-
 	std::cout << "Default path: " << path << std::endl;
-	/*FILE INTERPRETATION HERE ON THE FUTURE*/
 	(void)file;
+
 	this->addSocket(4242, path);
 	this->addSocket(8484, path);
 	this->addSocket(2121, path);
@@ -72,7 +58,7 @@ void	Server::init(std::string file) {
 
 void	Server::run(void) {
 	this->startListening();
-	std::cout << "Ports listening" << std::endl;
+	std::cout << "Ports are now listening" << std::endl;
 	while (1) {
 		poll(_polls.data(), _polls.size(), 1000);
 		this->handlePoll();
@@ -114,7 +100,6 @@ void	Server::handleSocket(struct pollfd spoll) {
 }
 
 void	Server::handleConnection(struct pollfd spoll) {
-	//std::cout << "handle" << std::endl;
 	if (spoll.revents & POLLIN)
 		_connections.at(spoll.fd)->readRequest();
 	if (spoll.revents & POLLOUT) {
@@ -136,7 +121,7 @@ void	Server::closeConnection(int fd) {
 	_changed = true;
 }
 
-// this is dumb, needs to be refactored later
+// maybe refactor this later?
 void	Server::organizePoll(void) {
 	struct pollfd	temp;
 	memset(&temp, 0, sizeof(temp));
