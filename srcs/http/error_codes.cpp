30bc555193e9910a559910f091c6025fbd4eee13
Lucas Fads <lucasfads@gmail.com>
2023-08-01 19:26:58 -0300

    fix: 405 with allow header

--- srcs/http/error_codes.cpp
+++ srcs/http/error_codes.cpp
@@ -2,7 +2,9 @@
 
 const char*	MethodNotAllowedException::what(void) const throw() {
 	return ( \
-		"HTTP/1.1 405 Method Not Allowed\r\n\r\n"							\
+		"HTTP/1.1 405 Method Not Allowed\r\n"								\
+		"Allow: GET, DELETE\r\n"											\
+		"\r\n"																\
 		"<html>"															\
 		"<body style=\"background-color:292D39\">"							\
 		"<div>"																\
