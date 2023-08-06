17b359dfcaca70db07fc1a5e88e5bfed782f7957
Lucas Fads <lucasfads@gmail.com>
2023-08-06 18:40:05 -0300

    chore: timeout exeption created

--- srcs/http/error_codes.cpp
+++ srcs/http/error_codes.cpp
@@ -1,5 +1,20 @@
 #include "error_codes.hpp"
 
+const char*	RequestTimeoutException::what(void) const throw() {
+	return ( \
+		"HTTP/1.1 408 Request Timeout\r\n"								\
+		"Content-Length: 185\r\n"											\
+		"Allow: GET, DELETE, POST\r\n"										\
+		"\r\n"																\
+		"<html>"															\
+		"<body style=\"background-color:292D39\">"							\
+		"<div>"																\
+		"<h1 align=\"center\", style=\"color:00BABC\">408 Request Timeout</h1>"	\
+		"<p align=\"center\", style=\"color:00BABC\">W3B53RB3RU5 1.0</p>"	\
+		"</div></body></html>"												\
+	);
+}
+
 const char*	MethodNotAllowedException::what(void) const throw() {
 	return ( \
 		"HTTP/1.1 405 Method Not Allowed\r\n"								\
